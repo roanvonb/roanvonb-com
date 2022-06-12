ac680ea5ee7a2cb0371bc19b4e1f216af3477c0f
roanvonb <roanvonb@gmail.com>
2022-06-12 14:38:58 +1000

    added helmet and compression

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('./prod')(app);
+
 app.listen(port,() => {
     console.log(`Listening on port ${port}`);
 })
